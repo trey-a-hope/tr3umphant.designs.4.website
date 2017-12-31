9d9ceb56f950653747be70f8a60b9e956d37b9ee
Trey Hope <treyhope@Treys-MacBook-Air.local>
2017-12-31 18:34:56 -0500

    Updated email endpoint to receive from variable.

--- php/sendEmail.php
+++ php/sendEmail.php
@@ -1,18 +1,14 @@
 <?php
-	$rest_json = file_get_contents("php://input");
-	$_POST = json_decode($rest_json, true);
+	$to      = $_POST['to'];
+	$subject = $_POST['subject'];
+	$message = $_POST['message'];
+	$headers = 'From: '.$_POST['from'];
 
-	/* Get passed parameters. */
-	$to = $_POST['to'];
-    $subject = $_POST['subject'];
-	$body = $_POST['body'];
+	$result = mail($to, $subject, $message, $headers);
 
-	/* Send email. */
-	$result = @mail($to, $subject, $body);
-
-    if($result){
-		print json_encode($result);
+	if($result){
+			print json_encode($result);
 	}else{
 		die(json_encode(array('message' => 'ERROR', 'code' => 1337)));
 	}   
-?>
+?> 
